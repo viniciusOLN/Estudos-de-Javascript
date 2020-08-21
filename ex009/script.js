a6543007eed68ad88c14897dbec8e5be21814d38
Vinicius Oliveira <vinicius853o@gmail.com>
2020-08-20 21:54:49 -0300

    Resolução de Atividade - Implementação da verificação de hora do dia e alteração da imagem

--- ex009/script.js
+++ ex009/script.js
@@ -1,5 +1,7 @@
 
 var hora = document.querySelector('.hora p')
+var img = document.querySelector('.imagem .img')
+var estado = document.querySelector('.imagem .estado')
 
 const zeroFill = n => {
     //retorna o horário indicado com um zero no começo se o valor for menor que 10
@@ -10,4 +12,20 @@ const intervalo = setInterval(() => {
     var date = new Date()
     hora.innerText = `A hora agora é de ${zeroFill(date.getHours())}:${zeroFill(date.getMinutes())}:${zeroFill(date.getSeconds())}`
 }, 1000)
+
+var date = new Date().getHours()
+
+if(date > 6 && date < 12){
+    img.src = '/ex009/image/tarde.png'
+    img.alt = 'Dia'
+    estado.innerText = 'No momento está de dia!'
+}else if(date > 12 && date < 18){
+    img.src = '/ex009/image/dia.png'
+    img.alt = 'Tarde'
+    estado.innerText = 'No momento está de tarde!'
+}else{
+    img.src = '/ex009/image/noite.png'
+    img.alt = 'Noite'
+    estado.innerText = 'No momento está de noite!'
+}
 
